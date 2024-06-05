83366cf0486b39176efc3ad18b138d4b2fb4b45d
giantcard5 <giantcard5.dev@outlook.com>
2024-06-05 12:37:53 -0300

    Updated Pesquisa

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,3 +8,11 @@ links.forEach((link) => {
         link.classList.add('active');
     }
 });
+
+function openSearch(artigo) {
+    var article = document.getElementById(`continuacao-artigo-${artigo}`);
+    var button = document.getElementById(`continuacao-artigo-${artigo}-button`);
+
+    article.style.display == 'none' ? article.style.display = 'block' : article.style.display = 'none';
+    article.style.display == 'none' ? button.innerText = 'Ler mais' : button.innerText = 'Ler menos';
+};
